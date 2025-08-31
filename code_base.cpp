ccd5b7fbc12b9abd4b51befe1caabff8ca99b5dc
aldair <aldair.gutierrez.h@uni.pe>
2025-08-31 00:19:14 -0500

    second test for pull

--- code_base.cpp
+++ code_base.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
-main(){
-cout<<"fucking you madda fucka "<<endl;
-}
+int main(){
+int xd;
+int a,b,n;
+cout<<"porfavor coloca la cantidad de elementos que va a evaluar"<<endl;
+cin>>n;
+for(int k=1;k<=n;k++) {
+    if(k==1){ cout<<"coloque los numeros"<<endl;
+        cin>>a>>b;}else{
+            b=xd; cout<<"coloque el otro numero"<<endl;
+            cin>>a;
+        }
+    while(b!=0) {
+        xd=b;
+        b=a%b;
+        a=xd;
+    }
+}   cout<<"el mcd de los numeros es : "<<xd<<endl;
+    return 0;}
+// probando hacer un pull a mi repositorio de git hub
